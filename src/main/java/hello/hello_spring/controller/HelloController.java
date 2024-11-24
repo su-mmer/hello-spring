32a27f8daf4d0c388e984976c869067782574847
su-mmer <heekim987@gmail.com>
2024-11-25 01:27:32 +0900

    add mvc pattern and static

---
+++ src/main/java/hello/hello_spring/controller/HelloController.java
@@ -0,0 +1,24 @@
+package hello.hello_spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "hello!!");
+        return "hello";  // resources:templates의 hello 를 찾아서 Return
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+}
